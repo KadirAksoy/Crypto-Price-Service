5451531212dca31699bfe3ce8f666ca16d8658f5
Kadir Baran Aksoy <kadirbaranaksoy@icloud.com>
2023-04-12 11:02:23 +0300

    Service added

---
+++ src/main/java/com/kadiraksoy/CryptoPriceService/service/CryptoService.java
@@ -0,0 +1,29 @@
+package com.kadiraksoy.CryptoPriceService.service;
+
+
+import com.kadiraksoy.CryptoPriceService.dto.AvgPriceDto;
+import com.kadiraksoy.CryptoPriceService.dto.AvgPriceDtoConverter;
+import com.kadiraksoy.CryptoPriceService.model.AvgPrice;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CryptoService {
+
+    private final RestTemplate restTemplate;
+    private final AvgPriceDtoConverter converter;
+    private final String API_URL = "https://api.binance.com/api/v3/avgPrice?symbol=";
+
+    public CryptoService(RestTemplate restTemplate, AvgPriceDtoConverter converter) {
+        this.restTemplate = restTemplate;
+        this.converter = converter;
+    }
+
+
+    public AvgPriceDto getCrypto(String symbol){
+        AvgPrice avgPrice = restTemplate.getForObject(API_URL+ symbol +"USDT", AvgPrice.class);
+        avgPrice.setSymbol(symbol);
+        return converter.convert(avgPrice);
+
+    }
+}
